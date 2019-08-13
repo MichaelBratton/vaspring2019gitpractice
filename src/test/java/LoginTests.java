f85c3c2c8f4d6ff5a313a97faa843f71fb396d55
Michael Bratton <bratonmike@icloud.com>
2019-08-13 19:36:24 -0400

    LoginTets

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,17 @@
+public class LoginTests {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("LOGIN Tets Running");
+        Thread.sleep(1000);
+        System.out.println(".");
+        Thread.sleep(1000);
+        System.out.println("..");
+        Thread.sleep(1000);
+        System.out.println("...");
+        Thread.sleep(1000);
+        System.out.println(".....");
+        Thread.sleep(1000);
+        System.out.println("......");
+        Thread.sleep(1000);
+        System.out.println("Tets PASSED");
+}
+}
