db28b3633c4c82750bb57e95eb679a393c239246
Michael Bratton <bratonmike@icloud.com>
2019-08-13 21:31:20 -0400

    devcommit2

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,7 @@
 public class AppTest {
     public static void main(String[] args) {
         System.out.println("hello world, this is development branch");
+        System.out.println("some new code had been written");
 
     }
 }
