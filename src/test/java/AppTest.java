3bf8a1fec87dfd1fea81f01e3a1054756a3681a6
Michael Bratton <bratonmike@icloud.com>
2019-08-13 20:05:22 -0400

    Test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,7 @@
 public class AppTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("making a change test");
+        System.out.println("making a change testt");
 
     }
 }
