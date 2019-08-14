886b88e35541951fb279825d4ccc97d61a35299f
Michael Bratton <bratonmike@icloud.com>
2019-08-13 21:24:06 -0400

    commit 3

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,6 @@
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("IM A BOSS BRO< SHOW SOME RESPEK");
+        System.out.println("hello world");
 
     }
 }
