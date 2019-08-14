86651c7fb5d372e12ec92e5c9e38181ade1ee25a
Michael Bratton <bratonmike@icloud.com>
2019-08-13 21:40:35 -0400

    newrepopush1

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,6 +2,7 @@ public class AppTest {
     public static void main(String[] args) {
         System.out.println("hello world, this is development branch");
         System.out.println("some new code had been written");
+        System.out.println("Even more code has been added");
 
     }
 }
