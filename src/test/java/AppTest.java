7181a1b81ee2bafd70f3bfc91184e5d8b3467e7f
Michael Bratton <bratonmike@icloud.com>
2019-08-13 21:28:06 -0400

    Development branch

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,6 @@
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world, this is development branch");
 
     }
 }
